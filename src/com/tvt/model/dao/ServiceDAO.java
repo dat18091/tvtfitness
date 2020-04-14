1623309fe08f390dda22fbf2f017e3dfb8b416c2
nguyen quang dat <dat18090@gmail.com>
2020-04-14 10:35:20 +0700

    add bo

---
+++ src/com/tvt/model/dao/ServiceDAO.java
@@ -0,0 +1,33 @@
+package com.tvt.model.dao;
+
+import java.util.List;
+
+import com.tvt.model.bean.Service;
+
+/**
+ * @author dat18
+ *
+ */
+public class ServiceDAO implements IDAOBase<Service> {
+
+	@Override
+	public List<Service> getAll() {
+		return null;
+	}
+
+	@Override
+	public void insert(Service service) {
+		
+	}
+
+	@Override
+	public void update(Service service) {
+		
+	}
+
+	@Override
+	public void delete(String serviceId) {
+		
+	}
+
+}
